1e880644abb12cbabab87fd71f8f32a6fbf706c8
Simos Gerasimou <simos.gerasimou@york.ac.uk>
2024-08-03 11:59:48 +0100

    cleanup

--- dummy/ConfigFileParser.py
+++
@@ -1,8 +0,0 @@
-from configparser import ConfigParser
-
-config = ConfigParser()
-config.read_file(open(r'configFIle.txt'))
-path1 = config.get('My Section', 'path1')
-path2 = config.get('My Section', 'path2')
-path3 = config.get('My Section', 'path3')
-
